1d9e164dcbec1a898f5fb11f740a8ab9f3559f59
felipecleverox <felipe.vasquez@cleverox.com>
2024-06-06 13:31:12 -0400

    MySQL
    creación estructura base de datos

---
+++ SQL files/Creacion evento semaforo.sql
@@ -0,0 +1,111 @@
+DELIMITER $$
+
+CREATE EVENT actualizar_beacon_status
+ON SCHEDULE EVERY 30 MINUTE
+STARTS CURRENT_TIMESTAMP
+DO
+BEGIN
+    DECLARE status_beacon_s1 VARCHAR(45);
+    DECLARE status_beacon_s2 VARCHAR(45);
+    DECLARE status_beacon_s3 VARCHAR(45);
+    DECLARE status_beacon_s4 VARCHAR(45);
+    DECLARE status_beacon_s5 VARCHAR(45);
+    DECLARE count_beacons INT;
+
+    -- ini sector 1 --
+    SELECT COUNT(*) into count_beacons
+    FROM gps_data
+    WHERE ble_beacons LIKE (select CONCAT('%',id, '%')  from beacons where lugar="Sector 1")
+    AND ident = 352592573522828 
+    AND timestamp > ((SELECT timestamp FROM gps_data ORDER BY id DESC LIMIT 1)- 1800);
+    insert into debug_beacon_count(sector,beacon_id,count,timestamp) values('sector 1', (select id from beacons where lugar like 'Sector 1'),count_beacons,NOW());
+
+    IF count_beacons = 0 THEN
+        SET status_beacon_s1 = 'Negro';
+    ELSEIF count_beacons < 15 THEN
+        SET status_beacon_s1 = 'Rojo';
+    ELSEIF count_beacons >= 15 AND count_beacons < 20 THEN
+        SET status_beacon_s1 = 'Amarillo';
+    ELSE
+        SET status_beacon_s1 = 'Verde';
+    END IF;
+
+    -- ini sector 2 --
+	SELECT COUNT(*) into count_beacons
+    FROM gps_data
+    WHERE ble_beacons LIKE (select CONCAT('%',id, '%')  from beacons where lugar="Sector 2")
+    AND ident = 352592573522828 
+    AND timestamp > ((SELECT timestamp FROM gps_data ORDER BY id DESC LIMIT 1)- 1800);
+    insert into debug_beacon_count(sector,beacon_id,count,timestamp) values('sector 2', (select id from beacons where lugar like 'Sector 2'),count_beacons,NOW());
+
+    IF count_beacons = 0 THEN
+        SET status_beacon_s2 = 'Negro';
+    ELSEIF count_beacons < 15 THEN
+        SET status_beacon_s2 = 'Rojo';
+    ELSEIF count_beacons >= 15 AND count_beacons < 20 THEN
+        SET status_beacon_s2 = 'Amarillo';
+    ELSE
+        SET status_beacon_s2 = 'Verde';
+    END IF;
+
+    -- ini sector 3 --
+	SELECT COUNT(*) into count_beacons
+    FROM gps_data
+    WHERE ble_beacons LIKE (select CONCAT('%',id, '%')  from beacons where lugar="Sector 3")
+    AND ident = 352592573522828 
+    AND timestamp > ((SELECT timestamp FROM gps_data ORDER BY id DESC LIMIT 1)- 1800);
+    insert into debug_beacon_count(sector,beacon_id,count,timestamp) values('sector 3', (select id from beacons where lugar like 'Sector 3'),count_beacons,NOW());
+
+    IF count_beacons = 0 THEN
+        SET status_beacon_s3 = 'Negro';
+    ELSEIF count_beacons < 15 THEN
+        SET status_beacon_s3 = 'Rojo';
+    ELSEIF count_beacons >= 15 AND count_beacons < 20 THEN
+        SET status_beacon_s3 = 'Amarillo';
+    ELSE
+        SET status_beacon_s3 = 'Verde';
+    END IF;
+
+    -- ini sector 4 --
+	SELECT COUNT(*) into count_beacons
+    FROM gps_data
+    WHERE ble_beacons LIKE (select CONCAT('%',id, '%')  from beacons where lugar="Sector 4")
+    AND ident = 352592573522828 
+    AND timestamp > ((SELECT timestamp FROM gps_data ORDER BY id DESC LIMIT 1)- 1800);
+    insert into debug_beacon_count(sector,beacon_id,count,timestamp) values('sector 4', (select id from beacons where lugar like 'Sector 4'),count_beacons,NOW());
+
+    IF count_beacons = 0 THEN
+        SET status_beacon_s4 = 'Negro';
+    ELSEIF count_beacons < 15 THEN
+        SET status_beacon_s4 = 'Rojo';
+    ELSEIF count_beacons >= 15 AND count_beacons < 20 THEN
+        SET status_beacon_s4 = 'Amarillo';
+    ELSE
+        SET status_beacon_s4 = 'Verde';
+    END IF;
+
+    -- ini sector 5 --
+     SELECT COUNT(*) into count_beacons
+    FROM gps_data
+    WHERE ble_beacons LIKE (select CONCAT('%',id, '%')  from beacons where lugar="Sector 5")
+    AND ident = 352592573522828 
+    AND timestamp > ((SELECT timestamp FROM gps_data ORDER BY id DESC LIMIT 1)- 1800);
+    insert into debug_beacon_count(sector,beacon_id,count,timestamp) values('sector 5', (select id from beacons where lugar like 'Sector 5'),count_beacons,NOW());
+
+    IF count_beacons = 0 THEN
+        SET status_beacon_s5 = 'Negro';
+    ELSEIF count_beacons < 15 THEN
+        SET status_beacon_s5 = 'Rojo';
+    ELSEIF count_beacons >= 15 AND count_beacons < 20 THEN
+        SET status_beacon_s5 = 'Amarillo';
+    ELSE
+        SET status_beacon_s5 = 'Verde';
+    END IF;
+
+    -- Aquí puedes agregar el código adicional para procesar status_beacon
+    INSERT INTO beacons_detection_status(status_timestamp, Sector_1, Sector_2, Sector_3, Sector_4, Sector_5) 
+    VALUES (CURRENT_TIMESTAMP, status_beacon_s1, status_beacon_s2, status_beacon_s3, status_beacon_s4, status_beacon_s5);
+
+END$$
+
+DELIMITER ;
