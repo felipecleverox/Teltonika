a719d03e148993b6aae92445bb295ac47b76247e
Felipe Andrés Vásquez Tort <f.vasquez.tort@gmail.com>
2024-07-10 18:04:17 -0400

    posible solcuion de error en el back buton

--- src/utils/backButtonHandler.js
+++ src/utils/backButtonHandler.js
@@ -1,23 +1,28 @@
+// backButtonHandler.js
+
+import { useNavigate } from 'react-router-dom';
+
 export function handleBackButton() {
-    // Manejador del evento popstate
-    window.onpopstate = function(event) {
-      console.log("El usuario hizo clic en el botón Atrás");
-    };
-  
-    // Función para prevenir el comportamiento predeterminado y agregar lógica personalizada
-    const popStateHandler = function(event) {
-      event.preventDefault(); // Evita recargar la página anterior
-  
-      // Lógica personalizada: Redirigir a la página principal
-      window.location.href = "/";
-    };
-  
-    // Agregar el event listener
-    window.addEventListener('popstate', popStateHandler);
-  
-    // Función de limpieza para remover el event listener al desmontar
-    return () => {
-      window.removeEventListener('popstate', popStateHandler);
-    };
-  }
-  
+  const navigate = useNavigate(); // Obtén la función navigate de react-router-dom
+
+  // Manejador del evento popstate
+  window.onpopstate = function(event) {
+    console.log("El usuario hizo clic en el botón Atrás");
+  };
+
+  // Función para prevenir el comportamiento predeterminado y agregar lógica personalizada
+  const popStateHandler = function(event) {
+    event.preventDefault(); // Evita recargar la página anterior
+
+    // Lógica personalizada: Redirigir a SelectRoutine
+    navigate('/select-routine');
+  };
+
+  // Agregar el event listener
+  window.addEventListener('popstate', popStateHandler);
+
+  // Función de limpieza para remover el event listener al desmontar
+  return () => {
+    window.removeEventListener('popstate', popStateHandler);
+  };
+}
