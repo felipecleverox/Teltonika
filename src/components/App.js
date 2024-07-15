96beeb028ac875d0fe3bfea2ad17a7241db79376
felipecleverox <felipe.vasquez@cleverox.com>
2024-07-15 17:11:55 -0400

    solucion provicional de boton atras.
    Co-Authored-By: Felipe Andrés Vásquez Tort <felipevasquez.tort@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
+
+
 import React, { useEffect } from 'react';
-import { useNavigate, BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate, useLocation } from 'react-router-dom';
+
+
 import LastKnownPosition from '../LastKnownPosition';
 import UbicacionTiempoRealInteriores from '../UbicacionTiempoRealInteriores';
 import PersonSearch from '../PersonSearch';
@@ -16,54 +20,53 @@ import ForgotPassword from '../ForgotPassword';
 import ResetPassword from '../ResetPassword';
 import Dashboard from '../Dashboard';
 import Temperatura from '../Temperatura'; 
-
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem('token');
   return token ? children : <Navigate to="/" />;
 };
 
-function App() {
+function AppRoutes() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    const onPopState = () => {
-      console.log("El usuario hizo clic en el botón Atrás");
+    const handleRouteChange = () => {
+      const token = localStorage.getItem('token');
+      if (token && location.pathname === '/') {
+        navigate('/select-routine');
+      }
     };
 
-    const popStateHandler = (event) => {
-      event.preventDefault(); // Prevenir la recarga de la página anterior
-      navigate('/select-routine'); // Redireccionar a SelectRoutine
-    };
-
-    window.onpopstate = onPopState;
-    window.addEventListener('popstate', popStateHandler);
-
-    return () => {
-      window.removeEventListener('popstate', popStateHandler);
-    };
-  }, [navigate]);
+    handleRouteChange();
+  }, [location, navigate]);
 
   return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+      <Route path="/select-routine" element={<PrivateRoute><SelectRoutine /></PrivateRoute>} />
+      <Route path="/last-known-position" element={<PrivateRoute><LastKnownPosition /></PrivateRoute>} />
+      <Route path="/ubicaciones-interior" element={<PrivateRoute><UbicacionTiempoRealInteriores /></PrivateRoute>} />
+      <Route path="/busqueda-entradas-persona" element={<PrivateRoute><PersonSearch /></PrivateRoute>} />
+      <Route path="/consulta-historica-movimientos" element={<PrivateRoute><HistoricalMovementsSearch /></PrivateRoute>} />
+      <Route path="/door-status-matrix" element={<PrivateRoute><DoorStatusMatrix /></PrivateRoute>} />
+      <Route path="/inteligencia-de-datos" element={<PrivateRoute><DataIntelligence /></PrivateRoute>} />
+      <Route path="/sms-data" element={<PrivateRoute><SmsData /></PrivateRoute>} />
+      <Route path="/presencia" element={<PrivateRoute><Presencia /></PrivateRoute>} />
+      <Route path="/configuracion" element={<PrivateRoute><Configuration /></PrivateRoute>} />
+      <Route path="/register-user" element={<PrivateRoute><UserRegistration /></PrivateRoute>} />
+      <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+      <Route path="/temperatura" element={<PrivateRoute><Temperatura /></PrivateRoute>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+}
+
+function App() {
+  return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/select-routine" element={<PrivateRoute><SelectRoutine /></PrivateRoute>} />
-        <Route path="/last-known-position" element={<PrivateRoute><LastKnownPosition /></PrivateRoute>} />
-        <Route path="/ubicaciones-interior" element={<PrivateRoute><UbicacionTiempoRealInteriores /></PrivateRoute>} />
-        <Route path="/busqueda-entradas-persona" element={<PrivateRoute><PersonSearch /></PrivateRoute>} />
-        <Route path="/consulta-historica-movimientos" element={<PrivateRoute><HistoricalMovementsSearch /></PrivateRoute>} />
-        <Route path="/door-status-matrix" element={<PrivateRoute><DoorStatusMatrix /></PrivateRoute>} />
-        <Route path="/inteligencia-de-datos" element={<PrivateRoute><DataIntelligence /></PrivateRoute>} />
-        <Route path="/sms-data" element={<PrivateRoute><SmsData /></PrivateRoute>} />
-        <Route path="/presencia" element={<PrivateRoute><Presencia /></PrivateRoute>} />
-        <Route path="/configuracion" element={<PrivateRoute><Configuration /></PrivateRoute>} />
-        <Route path="/register-user" element={<PrivateRoute><UserRegistration /></PrivateRoute>} />
-        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-        <Route path="/temperatura" element={<PrivateRoute><Temperatura /></PrivateRoute>} /> 
-        <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+      <AppRoutes />
     </Router>
   );
 }
