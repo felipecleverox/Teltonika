24f23a51bf7876d61d456f66d03400c096f25443
felipecleverox <felipe.vasquez@cleverox.com>
2024-07-24 18:07:24 -0400

    Agregado Menu Lateral

---
+++ src/SideNav.js
@@ -0,0 +1,69 @@
+// SideNav.js
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './SideNav.css';
+
+// Importa las imágenes y datos de las rutinas
+import dashboardImage from './assets/images/dashboard.png';
+import ubicaciontiemporealinteriorImage from './assets/images/ubicaciontiemporealinterior.png';
+import personSearchImage from './assets/images/person_search.png';
+import interiorLocationsImage from './assets/images/plano_super.png';
+import historicalMovementsImage from './assets/images/historical_movements1.png';
+import dataIntelligenceImage from './assets/images/data_intelligence.png';
+import configurationImage from './assets/images/configuration.png';
+import presenciaImage from './assets/images/presencia.png';
+import smsDataImage from './assets/images/sms_data.png';
+import doorStatusImage from './assets/images/door_status.png';
+import userRegistrationImage from './assets/images/user_registration.png';
+import temperaturaImage from './assets/images/temperatura.png';
+import selectRoutineImage from './assets/images/selectroutine.png';
+
+const routines = [
+  { title: "Seleccionar Rutina", image: selectRoutineImage, route: "/select-routine", permission: "view_dashboard" },
+  { title: "Dashboard", image: dashboardImage, route: "/dashboard", permission: "view_dashboard" },
+  { title: "Interior : Ubicación en Tiempo Real", image: ubicaciontiemporealinteriorImage, route: "/ubicaciones-interior", permission: "view_interior" },
+  { title: "Interior : Búsqueda Histórica Ubicación", image: personSearchImage, route: "/busqueda-entradas-persona", permission: "search_interior" },
+  { title: "Exterior : Ubicación Tiempo Real", image: interiorLocationsImage, route: "/last-known-position", permission: "view_exterior" },
+  { title: "Exterior : Búsqueda Histórica Ubicación", image: historicalMovementsImage, route: "/consulta-historica-movimientos", permission: "search_exterior" },
+  { title: "Sectores : Presencia Personal", image: presenciaImage, route: "/presencia", permission: "view_presence" },
+  { title: "Mensajes SOS : Visualización por Ubicación", image: smsDataImage, route: "/sms-data", permission: "view_sms" },
+  { title: "Temperatura", image: temperaturaImage, route: "/temperatura", permission: "view_temperature" },
+  { title: "Puertas : Status Cierre / Apertura", image: doorStatusImage, route: "/door-status-matrix", permission: "view_door_status" },
+  { title: "Datos : Análisis Forense", image: dataIntelligenceImage, route: "/inteligencia-de-datos", permission: "view_data_intelligence" },
+  { title: "Parametrización", image: configurationImage, route: "/configuracion", permission: "view_configuration" },
+  { title: "Registrar Usuario", image: userRegistrationImage, route: "/register-user", permission: "create_users" }
+];
+
+const SideNav = ({ userPermissions }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const location = useLocation();
+
+  const filteredRoutines = routines.filter(routine => 
+    userPermissions.includes(routine.permission)
+  );
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <nav className={`side-nav ${isExpanded ? 'expanded' : ''}`}>
+      <button className="expand-button" onClick={toggleExpand}>
+        {isExpanded ? '<<' : '>>'}
+      </button>
+      {filteredRoutines.map((routine, index) => (
+        <Link 
+          to={routine.route} 
+          key={index} 
+          className={`side-nav-item ${location.pathname === routine.route ? 'active' : ''}`}
+          title={routine.title}
+        >
+          <img src={routine.image} alt={routine.title} className="side-nav-icon" />
+          {isExpanded && <span className="side-nav-title">{routine.title}</span>}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default SideNav;
